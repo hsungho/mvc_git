a8441ed222f0a4df78317cd7df55a72a305a4ce6
westgad@gmail.com <westgad@gmail.com>
2016-07-20 14:28:53 +0900

    2:28

---
+++ src/global/JDBCTest.java
@@ -0,0 +1,39 @@
+package global;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.Statement;
+
+/**
+ * @date   :2016. 6. 30.
+ * @author :ckan
+ * @file   :JDBCTest.java 
+ * @story  :
+ */
+public class JDBCTest {
+	public static void main(String[] args) {
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		String sql = "select * from member where id = 'IU' ",result="";
+		List<String> list = new ArrayList<String>();
+		try {
+			Class.forName(Constants.ORACLE_DRIVER);
+			con = DriverManager.getConnection(Constants.ORACLE_URL,Constants.USER_ID,Constants.USER_PW);
+			stmt = con.createStatement();
+			
+			rs = stmt.executeQuery(sql); 
+			while (rs.next()) {
+				result = "id : "+rs.getString("ID")+" pw : "+rs.getString("pw");
+				list.add(result);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		System.out.println(list);
+	}
+}
---
+++ src/home/HomeController.java
@@ -0,0 +1,39 @@
+package home;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HomeController
+ */
+@WebServlet({"/home.do"})
+public class HomeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+ 
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("서블릿 입장..");
+        String servletpath = request.getServletPath();
+        System.out.println("서블릿 경로"+servletpath);
+        String path = servletpath.split("/")[1];
+        String view = path.substring(0, path.indexOf("."));
+        System.err.println("가야할 경로 : "+ view);
+			RequestDispatcher dis = request.getRequestDispatcher("/WEB-INF/global/"+view+".jsp");
+		dis.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
